32ce88fe020a87a0a39c6ec8c289578e6b3b3fc5
Juan Carlos Giler <jcgiler@in-planet.net>
2016-07-12 15:25:11 -0500

    Initial Commit

--- apps/home/urls.py
+++ apps/home/urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls import url
-from .views import HomeContent, VideoContent
+from .views import HomeContent
 
 urlpatterns = [
-    url(r'^video/', VideoContent),
     url(r'^', HomeContent)
 ]
--- apps/home/views.py
+++ apps/home/views.py
@@ -11,21 +11,29 @@ import subprocess
 
 def HomeContent(request):
     template = 'home/home.html'
-    return render(request, template, {})
 
-def VideoContent(request):
-    template = 'home/video.html'
-    url = request.GET['url']
+    if request.GET:
+        url = request.GET['url']
+        option = request.GET['option']
 
-    ydl_opts = {
-            'outtmpl': os.path.join(settings.MEDIA_ROOT, '%(title)s.%(ext)s')
-            }
+        ydl_opts = {
+                'outtmpl': os.path.join(settings.MEDIA_ROOT, '%(title)s.%(ext)s')
+                }
 
-    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        ydl.download([str(url)])
-        meta = ydl.extract_info(str(url), download=False)
-
-    if request.method == 'GET':
-        return render(request, template, { 'title': meta['title'],
-            'ext': meta['ext']
-            })
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            try:
+                ydl.download([str(url)])
+                meta = ydl.extract_info(str(url), download=False)
+                if request.method == 'GET':
+                    return render(request, template, {
+                            'title': meta['title'],
+                            'ext': meta['ext'],
+                            'display_success': 'block',
+                        })
+            except:
+                return render(request, template, {
+                        'error': 'Url incorrecta, prueba de nuevo con otra...',
+                        'display_error': 'block',
+                    })
+    else:
+        return render(request, template, {})
